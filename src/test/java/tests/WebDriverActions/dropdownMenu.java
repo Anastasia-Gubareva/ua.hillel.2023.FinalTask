51612f6fdb4d14325aa200582549ff3f37c03dd1
Gubareva Anastasya <Gubareva.Anastasya@gmail.com>
2023-07-24 15:20:01 +0300

    HoweWork   23 add Report

--- src/test/java/tests/WebDriverActions/dropdownMenu.java
+++ src/test/java/tests/WebDriverActions/dropdownMenu.java
@@ -1,5 +1,6 @@
 package tests.WebDriverActions;
 
+import hillel.listeners.CustomExtentReportListener;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,10 +10,18 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+
+
+@Listeners(CustomExtentReportListener.class)
 public class dropdownMenu {
 
+    static {
+        System.setProperty("extent.reporter.html.start", "true");
+        System.setProperty("extent.reporter.html.out", "target/extentReport/ExtentHtml.html");
+    }
     WebDriver driver = new ChromeDriver();
 
     @BeforeTest
